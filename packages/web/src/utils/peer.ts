116736ed0b876e9106325f0fd970ebd2c524e78c
yuchiXiong <yuchi.xiong@foxmail.com>
2024-08-23 17:01:57 +0800

    feat: update Peer Options to local

--- packages/web/src/utils/peer.ts
+++ packages/web/src/utils/peer.ts
@@ -18,8 +18,8 @@ let peerInstance: Peer | null = null;
 
 export const getPeerInstance = (id: string, standalone = true) => {
   const options = {
-    host: '116.62.176.240',
-    port: 80,
+    host: '127.0.0.1',
+    port: 9000,
     path: "/myapp",
   };
   if (!standalone) {
