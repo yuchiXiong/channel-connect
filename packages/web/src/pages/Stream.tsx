6a12479147ce86d9ec2c20597b6f47fb121e65a3
yuchi Xiong <yuchi.xiong@foxmail.com>
2024-12-06 23:09:48 +0800

    feat: 进一步将文件分片调整为文件流传输

--- packages/web/src/pages/Stream.tsx
+++ packages/web/src/pages/Stream.tsx
@@ -21,7 +21,7 @@ const StreamPage = () => {
     current: 0,
     type: ''
   });
-  const fileRef = useRef<ArrayBuffer>();
+  const fileRef = useRef<Record<string, Uint8Array>>({});
 
   const usp = new URLSearchParams(location.search);
 
@@ -31,30 +31,48 @@ const StreamPage = () => {
 
     const file = files[0];
 
+
     // 创建 webRTC 连接
     const peer = new Peer('sender', {
       host: "116.62.176.240",
       port: 80,
       path: "/myapp",
     })
-    peer.on("open", (id) => {
+    peer.on("open", async (id) => {
       console.log("My peer ID is: " + id);
 
-      const conn = (peer as Peer).connect('receiver');
+      const conn = peer.connect('receiver');
 
-      file.arrayBuffer().then((buffer) => {
-        const fileId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        const fileName = file.name;
-        const fileSize = buffer.byteLength;
-        const chunkSize = 1024 * 1024 * 10;
-        conn.send({ fileId, fileName, fileSize, flag: 'start', type: file.type });
-        // 分片发送
-        for (let i = 0; i < buffer.byteLength; i += chunkSize) {
-          const chunk = buffer.slice(i, i + chunkSize);
-          conn.send({ fileId, fileName, chunk, type: file.type, flag: 'chunk', start: i });
+      const fileId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      const fileName = file.name;
+      const fileSize = file.size;
+      
+      const stream = file.stream();
+      const reader = stream.getReader();
+
+      const readChunk = async (index: number) => {
+        const { done, value } = await reader.read(); // 读取下一块数据
+        if (done) {
+          conn.send({ fileId, fileName, fileSize, flag: 'end', type: file.type });
+          return;
         }
-        conn.send({ fileId, fileName, fileSize, flag: 'end', type: file.type });
-      })
+
+        conn.send({ fileId, fileName, chunk: value, type: file.type, flag: 'chunk', index });
+
+        await new Promise((resolve) =>
+          setTimeout(resolve, 50)
+        );
+
+        readChunk(index + 1); // 递归读取下一块数据
+      }
+
+      conn.send({ fileId, fileName, fileSize, flag: 'start', type: file.type });
+      
+      await new Promise((resolve) =>
+        setTimeout(resolve, 50)
+      );
+
+      readChunk(0);
     });
 
   }
@@ -70,30 +88,26 @@ const StreamPage = () => {
 
       peer.on("connection", (conn) => {
         console.log(conn.peer);
-        conn.on("data", async (data) => {
+        conn.on("data", (data) => {
           const _data = data as {
             fileId: string;
             fileName: string;
             fileSize: number;
-            chunk?: ArrayBuffer;
+            chunk?: Uint8Array;
             flag: 'start' | 'end' | 'chunk';
             type: string;
-            start: number;
+            index: number;
           }
           console.log(data);
           // 合并文件
           if (_data.flag === 'start') {
-            // 初始化
-            const file = new File([new ArrayBuffer(0)], _data.fileName, { type: _data.type });
-            const buffer = await file.arrayBuffer()
-
-            console.log('init file', buffer)
-            fileRef.current = buffer;
+            console.log('init file')
+            fileRef.current = {};
             fileInfoRef.current = {
               fileId: _data.fileId,
               fileName: _data.fileName,
               fileSize: _data.fileSize,
-              current: buffer.byteLength,
+              current: 0,
               type: _data.type
             }
           } else if (_data.flag === 'end') {
@@ -101,23 +115,17 @@ const StreamPage = () => {
             console.log('end')
           } else {
             // 添加数据
-            const _file = new File([_data.chunk || new ArrayBuffer(0)], _data.fileName, { type: _data.type });
-            const buffer = await _file.arrayBuffer()
-
             if (fileRef.current === undefined) return;
+            if (!_data.chunk) return;
 
-            const newFile = new File([fileRef.current, buffer], _data.fileName, { type: _data.type });
-            const newFileBuffer = await newFile.arrayBuffer();
-
-            fileRef.current = newFileBuffer;
+            fileRef.current[_data.index] = _data.chunk;
             fileInfoRef.current = {
               fileId: _data.fileId,
               fileName: _data.fileName,
               fileSize: fileInfoRef.current.fileSize,
-              current: newFileBuffer.byteLength,
+              current: Object.values(fileRef.current).reduce((pre, cur) => pre + cur.byteLength, 0),
               type: _data.type
             }
-            console.log(newFileBuffer)
           }
         })
       })
@@ -136,13 +144,13 @@ const StreamPage = () => {
 
       {
         // fileInfoRef.current.current === fileInfoRef.current.fileSize &&
-        fileRef.current && <video key={counter} src={URL.createObjectURL(new Blob([fileRef.current], { type: fileInfoRef.current.type }))} controls height={400} width={320}></video>}
+        fileRef.current && <video key={counter} src={URL.createObjectURL(new Blob(Object.values(fileRef.current), { type: fileInfoRef.current.type }))} controls height={600} width={480}></video>}
 
 
       <span>FileName: {fileInfoRef.current.fileName}</span>
       <span>FileId: {fileInfoRef.current.fileId}</span>
       <span>FileSize: {fileInfoRef.current.fileSize / 1024 / 1024} MB</span>
-      <span>current: {fileInfoRef.current.current / 1024 / 1024} MB</span>
+      <span>current: {Object.values(fileRef.current).reduce((pre, cur) => pre + cur.byteLength, 0) / 1024 / 1024} MB</span>
       {/* <span>{Math.floor((file?.byteLength || 0) / 1024 / 1024)} MB</span> */}
     </section >
   )
